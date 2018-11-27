cb2bb88d2401d9f08111a8161c3e646ab3cf96de
Allen Ng <lukexng@gmail.com>
2018-11-27 15:40:11 +0800

    only download if not exist

--- download_opencv_native.sh
+++ download_opencv_native.sh
@@ -1,9 +1,13 @@
 DIR=`pwd`
-DESTINATION=$DIR/android/src/main
-curl -O https://jaist.dl.sourceforge.net/project/opencvlibrary/3.4.4/opencv-3.4.4-android-sdk.zip
+DESTINATION=$DIR/android/src/main/jniLibs
 
-unzip opencv-3.4.4-android-sdk.zip
-mkdir $DIR/android/src/main/jniLibs
-cp -r $DIR/OpenCV-android-sdk/sdk/native/libs/ $DESTINATION/jniLibs
-rm -rf opencv-3.4.4-android-sdk.zip
-rm -rf OpenCV-android-sdk/
+if [ ! -d "$DESTINATION" ]; then
+  # Only download if destination does not exist
+  curl -O https://jaist.dl.sourceforge.net/project/opencvlibrary/3.4.4/opencv-3.4.4-android-sdk.zip
+
+  unzip opencv-3.4.4-android-sdk.zip
+  mkdir $DESTINATION
+  cp -r $DIR/OpenCV-android-sdk/sdk/native/libs/ $DESTINATION
+  rm -rf opencv-3.4.4-android-sdk.zip
+  rm -rf OpenCV-android-sdk/
+fi
