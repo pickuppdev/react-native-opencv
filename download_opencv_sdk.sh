212ef0280edc2e1ea43c5608fdf595dd1d15d59d
Allen Ng <lukexng@gmail.com>
2018-11-30 12:45:47 +0800

    improve install script

--- download_opencv_sdk.sh
+++ download_opencv_sdk.sh
@@ -2,26 +2,36 @@ DIR=`pwd`
 JNI_DESTINATION=$DIR/android/libraries/opencv/jniLibs
 JAVA_DESTINATION=$DIR/android/libraries/opencv/src/main
 
-if [ ! -d "$JNI_DESTINATION" ] || [ ! -d "$JAVA_DESTINATION" ]; then
+
+downloadSdk() {
   # Only download if destination does not exist
   curl -O https://jaist.dl.sourceforge.net/project/opencvlibrary/3.4.4/opencv-3.4.4-android-sdk.zip
 
   unzip opencv-3.4.4-android-sdk.zip
+}
 
-  if [ ! -d "$JNI_DESTINATION" ]; then
-    # copy jni
-    mkdir $JNI_DESTINATION
-    cp -r $DIR/OpenCV-android-sdk/sdk/native/libs/ $JNI_DESTINATION
+copySdk() {
+  # copy jni
+  if [ -d "$JNI_DESTINATION" ]; then
+    rm -rf $JNI_DESTINATION
   fi
-=
-  if [ ! -d "$JAVA_DESTINATION/java" ]; then
-    # copy java
-    mkdir $JAVA_DESTINATION
-    cp -r $DIR/OpenCV-android-sdk/sdk/java/src/ $JAVA_DESTINATION/java
-    cp -r $DIR/OpenCV-android-sdk/sdk/java/res $JAVA_DESTINATION/res
-    cp -r $DIR/OpenCV-android-sdk/sdk/java/AndroidManifest.xml $JAVA_DESTINATION/AndroidManifest.xml
-  fi
+  mkdir $JNI_DESTINATION
+  cp -r $1/sdk/native/libs/ $JNI_DESTINATION
+  # copy java
+  cp -r $1/sdk/java/src/ $JAVA_DESTINATION/java
+  cp -r $1/sdk/java/res $JAVA_DESTINATION/res
+  cp -r $1/sdk/java/AndroidManifest.xml $JAVA_DESTINATION/AndroidManifest.xml
+}
 
+cleanupSdk() {
   rm -rf opencv-3.4.4-android-sdk.zip
   rm -rf OpenCV-android-sdk/
+}
+
+if [[ -z "${OPENCV_ANDROID_SDK}" ]] && [[ -d "$OPENCV_ANDROID_SDK" ]]; then
+  downloadSdk
+  copySdk $DIR/OpenCV-android-sdk
+  cleanupSdk
+else
+  copySdk $OPENCV_ANDROID_SDK
 fi
